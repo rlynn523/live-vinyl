a8378155b1137522b14c1ccbe8bf3d580dab72b5
Ryan Lynn <ryanlynn523@gmail.com>
2016-10-06 23:17:02 -0400

    refactored artist reducer after switched from AJAX to fetch in action

--- src/reducers/artist.js
+++ src/reducers/artist.js
@@ -4,7 +4,7 @@ import initialState from './initialstate';
 export default function ArtistReducer (state = initialState, action) {
     switch(action.type) {
         case actions.FETCH_ARTIST_ALBUMS:
-        let vinyl = action.vinyl.data.results;
+        let vinyl = action.vinyl.results;
         let Artist = Object.assign({}, state, {
             vinyl: vinyl
         });
