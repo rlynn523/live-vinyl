d008763ed88b9261b7977ba3e607fa63df1df352
Ryan Lynn <ryanlynn523@gmail.com>
2016-10-06 23:16:05 -0400

    updated routes to include routes to sign in, create user, and search components

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router';
+import Landing from './components/landing';
+import CreateUser from './components/signup';
 import Search from './components/search';
 import Vinyl from './components/vinyl';
 import Music from './components/music';
-
+import Tour from './components/tour';
+import SignIn from './components/signin';
 
 const SearchContainer = function() {
     return (
         <div>
             <Search />
+            <Tour />
             <Music />
             <Vinyl />
         </div>
@@ -17,7 +21,11 @@ const SearchContainer = function() {
 
 let mainRouter = (
     <Router history={hashHistory}>
-        <Route path='/' component={SearchContainer} />
+        <Route path='/' component={Landing} />
+        <Route path='/sign-in' component={SignIn} />
+        <Route path='/create-user' component={CreateUser} />
+        <Route path='/search' component={SearchContainer} />
     </Router>
 );
+
 module.exports = mainRouter;
