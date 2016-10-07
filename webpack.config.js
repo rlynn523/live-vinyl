96315bfc440301d0a2994cc6e6ce7d8aedf3bb42
Ryan Lynn <ryanlynn523@gmail.com>
2016-10-06 23:12:40 -0400

    refactored webpack.config.js, package.json. added db to config

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,7 @@ var webpack = require('webpack');
 
 var packageData = require('./package.json');
 
-var minify = process.argv.indexOf('--minify') != -1;
-
 var filename = [packageData.name, packageData.version, 'js'];
-var plugins = [];
-
-if (minify) {
-    filename.splice(filename.length - 1, 0, 'min');
-    plugins.push(new webpack.optimize.UglifyJsPlugin());
-}
 
 module.exports = {
     entry: path.resolve(__dirname, packageData.main),
@@ -22,16 +14,15 @@ module.exports = {
     },
     devtool: 'source-map',
     module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          loader: 'babel',
-          query: {
-            presets: ['es2015', 'react']
-          }
-        }
-      ]
-    },
-    plugins: plugins
-}
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
