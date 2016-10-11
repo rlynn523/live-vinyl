67206bf8d13e55aba5220948df2722442bd6de6a
Ryan Lynn <ryanlynn523@gmail.com>
2016-10-11 10:36:28 -0400

    added save tour date action to tour action file

--- src/actions/tour.js
+++ src/actions/tour.js
@@ -4,7 +4,7 @@ let FETCH_TOUR_DATES = 'FETCH_TOUR_DATES';
 let fetchTourDates = function(tour) {
     return {
         type: FETCH_TOUR_DATES,
-        tour
+        tour: tour
     }
 }
 let fetchTour = function(userSearch, tour) {
@@ -33,5 +33,51 @@ let fetchTour = function(userSearch, tour) {
     }
 }
 
+let SAVE_TOUR_DATE = 'SAVE_TOUR_DATE';
+let saveTourDate = function(tour) {
+    return {
+        type: SAVE_TOUR_DATE,
+        tour: tour
+    }
+}
+
+let saveTour = function(tour) {
+    return function(dispatch) {
+        let url = '/tours';
+        return fetch(url, { method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: tour.title,
+                date: tour.formatted_datetime,
+                tickets: tour.ticket_url
+            })
+            }).then(function(response) {
+            if (response.status < 200 || response.status >= 300) {
+               var error = new Error(response.statusText)
+               error.response = response
+               throw error;
+           }
+           return response.json();
+       }).then(function(data) {
+           let tour = data;
+            if(data) {
+                dispatch(
+                    saveTourDate(tour)
+                )
+            }
+        })
+        .catch(function(error) {
+            return dispatch(
+                console.log(error)
+            )
+        })
+    }
+}
+
 exports.FETCH_TOUR_DATES = FETCH_TOUR_DATES;
 exports.fetchTour = fetchTour;
+exports.SAVE_TOUR_DATE = SAVE_TOUR_DATE;
+exports.saveTour = saveTour
