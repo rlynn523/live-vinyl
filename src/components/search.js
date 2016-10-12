4edc4cbe41352040d592bc4f657f5ad939204fc9
Ryan Lynn <ryanlynn523@gmail.com>
2016-10-12 18:09:18 -0400

    updated style with material ui

--- src/components/search.js
+++ src/components/search.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import * as action1 from '../actions/artist';
 import * as action2 from '../actions/vinyl';
 import * as action3 from '../actions/tour';
+import { MuiThemeProvider, RaisedButton } from 'material-ui';
 
 export class Search extends Component {
     constructor(props) {
@@ -19,22 +20,25 @@ export class Search extends Component {
         this.props.dispatch(
             action3.fetchTour(this.refs.userSearch.value)
         )
+        this.refs.userSearch.value = '';
     }
     render() {
         return(
-            <div className='bandSearch'>
-                <form>
-                    <input type='text' ref='userSearch'/>
-                    <button type='button' onClick={this.onClick}>Search</button>
-                </form>
-            </div>
+            <MuiThemeProvider>
+                <div className='bandSearch'>
+                    <p className='searchTitle' style={{fontWeight: 'lighter'}}>Search An Artist</p>
+                    <input type='text' ref='userSearch' placeholder=' ex: Radiohead' />
+                    <RaisedButton backgroundColor='#FF9800' onClick={this.onClick} label='Search Artist' labelColor='#FFFFFF'/>
+                </div>
+            </MuiThemeProvider>
         );
     }
 }
 let mapStateToProps = function(state, props) {
     return {
         vinylRecords: state.VinylReducer.vinyl,
-        music: state.MusicReducer.music
+        music: state.ArtistReducer.music,
+        tour: state.TourReducer.tour
     }
 }
 export default connect(mapStateToProps)(Search);
