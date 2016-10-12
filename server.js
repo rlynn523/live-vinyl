2d65f2d5bd5e3a3efb5a39acce87381c16cdab73
Ryan Lynn <ryanlynn523@gmail.com>
2016-10-12 17:59:01 -0400

    added isLoggedIn function to GET and POST methods for /vinyl and /tours,  added /logout, passport authentication

--- server.js
+++ server.js
@@ -1,43 +1,30 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var id = mongoose.Types.ObjectId();
 var jsonParser = bodyParser.json();
 var config = require('./config.js');
 var bcrypt = require('bcryptjs');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
+var passport = require('passport') , LocalStrategy = require('passport-local').Strategy;
+
+mongoose.connect('mongodb://localhost/music');
+
 app.use(express.static('build'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser('lisa'));
-mongoose.connect('mongodb://localhost/music');
-// var passport = require('passport') , LocalStrategy = require('passport-local').Strategy;
-// passport.use('local', new LocalStrategy(
-//   function(username, password, done) {
-//     User.findOne({ username: username }, function (err, user) {
-//       if (err) { return done(err); }
-//       if (!user) {
-//         return done(null, false, { message: 'Incorrect username.' });
-//         }
-//         passvalidatePassword(password, user.password, function(error){
-//             if(error) {
-//                 return done(null, false, { message: 'Incorrect password.' });
-//             } else {
-//                 return done(null, user);
-//             }
-//         });
-//     });
-//   }
-// ));
-// function isLoggedIn(req, res, next) {
-//     console.log('isLoggedIn', req.isAuthenticated());
-//     if (req.isAuthenticated())
-//         return next();
-//     res.sendStatus(401);
-// }
+app.use(cookieParser());
+app.use(session({ secret: 'lisa' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/');
+}
 // =====================================
 // Models ==============================
 // =====================================
@@ -50,118 +37,135 @@ var Vinyl = require('./src/models/vinyl');
 // Passport ============================
 // =====================================
 
-// function passvalidatePassword(password, userpassword, callback) {
-//     bcrypt.compare(password, userpassword, function(err, isValid) {
-//         if (err) {
-//             callback(err);
-//             return;
-//         }
-//         callback(null, isValid);
-//     });
-// }
-// passport.serializeUser(function(user, done) {
-//     console.log('SERIALIZE',user);
-//   done(null, user.id);
-// });
-//
-// passport.deserializeUser(function(id, done) {
-//     console.log('ID', id);
-//   User.findById(id, function(err, user) {
-//       console.log('USER', user);
-//     done(err, user);
-//   });
-// });
-//
-// app.use(session({ secret: 'lisa' }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+passport.use('local', new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+        }
+        passvalidatePassword(password, user.password, function(error){
+            if(error) {
+                return done(null, false, { message: 'Incorrect password.' });
+            } else {
+                return done(null, user);
+            }
+        });
+    });
+  }
+));
+
+function passvalidatePassword(password, userpassword, callback) {
+    bcrypt.compare(password, userpassword, function(err, isValid) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        callback(null, isValid);
+    });
+}
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
 
 // // =====================================
 // // Creating New Users ==================
 // // =====================================
 
-// app.post('/users', jsonParser, function(req, res) {
-//     if (!req.body) {
-//         return res.status(400).json({
-//             message: 'No request body'
-//         });
-//     }
-//     if (!('username' in req.body)) {
-//         return res.status(422).json({
-//             message: 'Missing field: username'
-//         });
-//     }
-//     var username = req.body.username;
-//     if (typeof username !== 'string') {
-//         return res.status(422).json({
-//             message: 'Incorrect field type: username'
-//         });
-//     }
-//     username = username.trim();
-//     if (username === '') {
-//         return res.status(422).json({
-//             message: 'Incorrect field length: username'
-//         });
-//     }
-//     if (!('password' in req.body)) {
-//         return res.status(422).json({
-//             message: 'Missing field: password'
-//         });
-//     }
-//     var password = req.body.password;
-//     if (typeof password !== 'string') {
-//         return res.status(422).json({
-//             message: 'Incorrect field type: password'
-//         });
-//     }
-//
-//     password = password.trim();
-//
-//     if (password === '') {
-//         return res.status(422).json({
-//             message: 'Incorrect field length: password'
-//         });
-//     }
-//     bcrypt.genSalt(10, function(err, salt) {
-//         if (err) {
-//             return res.status(500).json({
-//                 message: 'Internal server error'
-//             });
-//         }
-//         bcrypt.hash(password, salt, function(err, hash) {
-//             if (err) {
-//                 return res.status(500).json({
-//                     message: 'Internal server error'
-//                 });
-//             }
-//             var user = new User({
-//                 username: username,
-//                 password: hash
-//             });
-//             user.save(function(err) {
-//                 if (err) {
-//                     return res.status(500).json({
-//                         message: 'Internal server error'
-//                     });
-//                 }
-//                 return res.status(201).json({});
-//             });
-//         });
-//     });
-// });
-//
-// app.post('/login', passport.authenticate('local'), function(req, res) {
-//     console.log('LOGIN', req.user);
-//     console.log('isLoggedIn /LOGIN', req.isAuthenticated());
-//     return res.status(200).json({});
-// });
+app.post('/users', jsonParser, function(req, res) {
+    if (!req.body) {
+        return res.status(400).json({
+            message: 'No request body'
+        });
+    }
+    if (!('username' in req.body)) {
+        return res.status(422).json({
+            message: 'Missing field: username'
+        });
+    }
+    var username = req.body.username;
+    if (typeof username !== 'string') {
+        return res.status(422).json({
+            message: 'Incorrect field type: username'
+        });
+    }
+    username = username.trim();
+    if (username === '') {
+        return res.status(422).json({
+            message: 'Incorrect field length: username'
+        });
+    }
+    if (!('password' in req.body)) {
+        return res.status(422).json({
+            message: 'Missing field: password'
+        });
+    }
+    var password = req.body.password;
+    if (typeof password !== 'string') {
+        return res.status(422).json({
+            message: 'Incorrect field type: password'
+        });
+    }
+
+    password = password.trim();
+
+    if (password === '') {
+        return res.status(422).json({
+            message: 'Incorrect field length: password'
+        });
+    }
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal server error'
+            });
+        }
+        bcrypt.hash(password, salt, function(err, hash) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Internal server error'
+                });
+            }
+            var user = new User({
+                username: username,
+                password: hash
+            });
+            user.save(function(err) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Internal server error'
+                    });
+                }
+                return res.status(201).json({});
+            });
+        });
+    });
+});
+
+// =====================================
+// Log In & Log Out Routes =============
+// =====================================
+
+app.post('/login', passport.authenticate('local'), function(req, res) {
+    return res.status(200).json({});
+});
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
 
 // =====================================
 // Tour Routes =========================
 // =====================================
 
-app.get('/tours', function(req, res) {
-    Tour.find(function(err, tour) {
+app.get('/tours', isLoggedIn, function(req, res) {
+    Tour.find({userId: req.user._id}, function(err, tour) {
         if (err) {
             return res.status(500).json({
                 message: 'Internal Server Error'
@@ -170,19 +174,26 @@ app.get('/tours', function(req, res) {
         return res.json(tour);
     });
 });
-app.post('/tours', function(req, res) {
-    Tour.create({
-            title: req.body.title,
-            date: req.body.date,
-            tickets: req.body.tickets
-    }, function(err, tour) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Internal Server Error'
-                });
-            }
-            return res.status(201).json(tour);
+app.post('/tours', isLoggedIn, function(req, res) {
+    if(req.body) {
+        Tour.create({
+                title: req.body.title,
+                date: req.body.date,
+                tickets: req.body.tickets,
+                userId: req.user._id
+        }, function(err, tour) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Internal Server Error'
+                    });
+                }
+                return res.status(201).json(tour);
+            });
+    } else {
+        return res.status(500).json({
+            message: 'Internal Server Error'
         });
+    }
 })
 app.delete('/tours/:id', function(req, res) {
     Tour.remove({
@@ -202,8 +213,8 @@ app.delete('/tours/:id', function(req, res) {
 // Vinyl Routes ========================
 // =====================================
 
-app.get('/vinyl', function(req, res) {
-    Vinyl.find(function(err, vinyl) {
+app.get('/vinyl', isLoggedIn, function(req, res) {
+    Vinyl.find({userId: req.user._id}, function(err, vinyl) {
         if (err) {
             return res.status(500).json({
                 message: 'Internal Server Error'
@@ -213,19 +224,26 @@ app.get('/vinyl', function(req, res) {
     });
 });
 app.post('/vinyl', function(req, res) {
-    Vinyl.create({
-            title: req.body.title,
-            country: req.body.country,
-            year: req.body.year
-    }, function(err, vinyl) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Internal Server Error'
-                });
-            }
-            return res.status(201).json(vinyl);
+    if(req.body) {
+        Vinyl.create({
+                title: req.body.title,
+                country: req.body.country,
+                year: req.body.year,
+                userId: req.user._id
+        }, function(err, vinyl) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Internal Server Error'
+                    });
+                }
+                return res.status(201).json(vinyl);
+            });
+    } else {
+        return res.status(500).json({
+            message: 'Internal Server Error'
         });
-})
+    }
+});
 app.delete('/vinyl/:id', function(req, res) {
     Vinyl.remove({
             _id: req.params.id
@@ -239,5 +257,17 @@ app.delete('/vinyl/:id', function(req, res) {
             return res.status(200).json(vinyl);
         });
 });
+app.get('/breeds', isLoggedIn, function(req, res) {
+    Breed.find({userId: req.user._id}, function(err, breed) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            });
+        }
+        return res.json(breed);
+    });
+});
 
 app.listen(process.env.PORT || 8080, process.env.IP);
+
+module.exports.app = app;
