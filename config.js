5fea75b5a9beef47ec27b502032dc105624f7b56
Ryan Lynn <ryanlynn523@gmail.com>
2016-10-12 22:42:34 -0400

    updated config.js for heroku deploy

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var db = process.env.NODE_ENV === 'production' ?
-    'mongodb://localhost/music':
+    'mongodb://ryanlynn523:rango523@ds027409.mlab.com:27409/heroku_scd828cw' :
+    'mongodb://localhost/music';
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL || db;
 exports.PORT = process.env.PORT || 8080;
