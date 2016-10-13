bb18689d53f6746e55f7db8079b558f56e3fd23d
Ryan Lynn <ryanlynn523@gmail.com>
2016-10-12 22:25:12 -0400

    removed unused code

--- test/landing-test.js
+++ test/landing-test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import { should } from 'chai';
 should();
-import { Provider } from 'react-redux';
 import store from '../src/store';
 import Landing from '../src/components/landing';
 
