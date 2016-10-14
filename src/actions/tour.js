713ab8103bfa88f57f883668dc5c871562a9515a
Ryan Lynn <ryanlynn523@gmail.com>
2016-10-14 16:09:11 -0400

    switched from fetch to ajax to test in deploy

--- src/actions/tour.js
+++ src/actions/tour.js
@@ -10,22 +10,19 @@ let fetchTourDates = function(tour) {
 let fetchTour = function(userSearch, tour) {
     return function(dispatch) {
         let url = 'https://api.bandsintown.com/artists/'+userSearch+'/events.json?api_version=2.0&app_id=VINYL_COLLECTION';
-        return fetch(url, {mode: 'cors'}).then(function(response) {
-            if (response.status < 200 || response.status >= 300) {
-               var error = new Error(response.statusText)
-               error.response = response
-               throw error;
-           }
-           return response.json();
-       }).then(function(data) {
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'jsonp',
+            contentType: 'application/json',
+        }).done(function(data) {
             if(data) {
                 let tour = data;
                 dispatch(
                     fetchTourDates(tour)
                 )
             }
-        })
-        .catch(function(error) {
+        }).fail(function(error) {
             return dispatch(
                 console.log(error)
             )
