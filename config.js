2ec0085973da4c1ac21daf601e3b541795f8e908
Ryan Lynn <ryanlynn523@gmail.com>
2016-10-13 21:54:11 -0400

    updated config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var db = process.env.NODE_ENV === 'production' ?
-    'mongodb://ryanlynn523:rango523@ds027409.mlab.com:27409/heroku_scd828cw' :
+    'mongodb://ryanlynn:rango123@ds027409.mlab.com:27409/heroku_scd828cw' :
     'mongodb://localhost/music';
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL || db;
