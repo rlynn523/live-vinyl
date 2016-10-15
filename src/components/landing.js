33ef7abc219f79139d97a57940a7f34ee14863d0
Ryan Lynn <ryanlynn523@gmail.com>
2016-10-14 22:20:21 -0400

    removed Chip components, added blurb about what app can do

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { MuiThemeProvider, Chip, RaisedButton } from 'material-ui';
+import { MuiThemeProvider, Chip, RaisedButton, FontIcon } from 'material-ui';
+import Album from 'material-ui/svg-icons/av/album'
 import actions from '../actions/signIn';
 
 export default class Landing extends Component {
@@ -9,17 +10,16 @@ export default class Landing extends Component {
             <MuiThemeProvider>
                 <div className='landingMain'>
                     <p className='titleMain' style={{fontWeight: 'lighter'}}>Welcome To Live Vinyl</p>
-                    <p className='titleSecond'>Search An Artist You Love</p>
-                    <div className='landingChips'>
-                        <Chip style={{margin: '5px auto'}} backgroundColor={'#C8E6C9'}>Get Current Tour Dates & Save Tour Dates To Your Profile</Chip>
-                        <Chip style={{margin: '5px auto'}} backgroundColor={'#C8E6C9'}>Get Available Albums On Spotify</Chip>
-                        <Chip style={{margin: '5px auto'}} backgroundColor={'#C8E6C9'}>Get Vinyl Releases From Artist & Add Them To Your Collection</Chip>
-                        <Chip style={{margin: '5px auto'}} backgroundColor={'#C8E6C9'}>Get Related Artists</Chip>
-                    </div>
+                        <FontIcon className='vinylIcon'><Album style={{width: '150px', height: '150px'}}/></FontIcon>
                     <div className='landingButtons'>
                         <Link to={'/sign-in'}><RaisedButton className='signInButton' backgroundColor='#FF9800' labelColor='#FFFFFF' label='Sign In' /></Link>
                         <Link to={'/create-user'}><RaisedButton className='signUpButton' backgroundColor='#FF9800' labelColor='#FFFFFF' label='Create User' /></Link>
                         <Link to={'/search'}><RaisedButton className='searchButtonLanding' backgroundColor='#FF9800' labelColor='#FFFFFF' label='Search' /></Link>
+                    </div>
+                    <div className='landingAbout'>
+                        <p style={{fontWeight: 'lighter'}}>Get Related Artists || Get Current Tour Dates</p>
+                        <p style={{fontWeight: 'lighter'}}>Get Vinyl Releases From Artist || Get Available Albums On Spotify</p>
+                        <p style={{fontWeight: 'lighter'}}>Save Tours & Vinyl To Your Profile</p>
                     </div>
                 </div>
             </MuiThemeProvider>
